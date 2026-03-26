d2268da3c50b13936f6b5501b3da8b0ea17d237c
Elvin Rodrigues <elvinrodrigues3456@gmail.com>
2026-03-26 23:39:52 +0530

    updated db.go

--- backend/internal/database/db.go
+++ backend/internal/database/db.go
@@ -3,22 +3,28 @@ package database
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func ConnectDB() *sql.DB {
-	connStr := "host=localhost user=contacts_app dbname=contacts_manager sslmode=disable"
+	dbURL := os.Getenv("DATABASE_URL")
 
-	db, err := sql.Open("postgres", connStr)
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
 
+	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	err = db.Ping()
 	if err != nil {
 		log.Fatal("Database not reachable:", err)
 	}
+
 	log.Println("Connected to PostgreSQL")
 	return db
 }
